8274e28c7c2918bb9700cd8379e5d0b86cb5f421
aidar.shaidullin <ajdarshaydullin@gmail.com>
2022-04-14 16:53:08 +0300

    add db migrations control

---
+++ src/main/resources/db/changelog/changelog.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+--changeset resource_management:1
+create or replace view task_type_statistics as
+select tt.id,
+       stddev((t.fact_time - tt.constant_bias) / t.square) as sigma,
+       avg((t.fact_time - tt.constant_bias) / t.square)       e,
+       constant_bias,
+       man_hour_per_square_meter
+from task t
+         inner join task_type tt on t.type_id = tt.id
+group by tt.id;
